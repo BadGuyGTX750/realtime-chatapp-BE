45167a15435946057577a94c543741d17fe31d7c
BadGuyGTX750 <stefanstefan211@gmail.com>
2023-04-19 15:41:48 +0300

    Added static DependencyInjection Class for app infrastructure purposes

---
+++ chatapp/Infrastructure/DependencyInjection.cs
@@ -0,0 +1,13 @@
+﻿namespace chatapp.Infrastructure
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddInfrastructure(
+            this IServiceCollection services,
+            IConfiguration configuration)
+        {
+
+            return services;
+        }
+    }
+}
--- chatapp/Program.cs
+++ chatapp/Program.cs
@@ -1,4 +1,11 @@
+using chatapp.Infrastructure;
+
 var builder = WebApplication.CreateBuilder(args);
+
+// Add infrastructure (Repositories, Services, etc...)
+builder.Services.AddInfrastructure(builder.Configuration);
+
+
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
