0833a5a3b24b5cea6627375a84c6863cdbe959c9
BadGuyGTX750 <stefanstefan211@gmail.com>
2023-04-19 16:57:53 +0300

    Added UseConfiguration static method to clear up Program.cs

---
+++ chatapp/Infrastructure/AppConfiguration.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Rewrite;
+using Swashbuckle.AspNetCore.SwaggerUI;
+
+namespace chatapp.Infrastructure
+{
+    public static class AppConfiguration
+    {
+        public static WebApplication UseConfiguration(this WebApplication app)
+        {
+            // Use swagger UI
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI(options =>
+                {
+                    options.SwaggerEndpoint("/swagger/v1/swagger.json", "StefanBeam API V1 Docs");
+                    options.DocExpansion(DocExpansion.None);
+                    options.DefaultModelsExpandDepth(-1);
+                });
+                // Redirect to swagger page on application start
+                var option = new RewriteOptions();
+                option.AddRedirect("^$", "swagger");
+                app.UseRewriter(option);
+            }
+
+            // Configure the HTTP request pipeline.
+            app.UseHttpsRedirection();
+            app.UseRouting();
+            app.MapControllers();
+
+            return app;
+        }
+    }
+}
--- chatapp/Program.cs
+++ chatapp/Program.cs
@@ -3,31 +3,14 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Rewrite;
 using Swashbuckle.AspNetCore.SwaggerUI;
 
-var builder = WebApplication.CreateBuilder(args);
 
+var builder = WebApplication.CreateBuilder(args);
 // Add infrastructure (Repositories, Services, etc...)
 builder.Services.AddInfrastructure(builder.Configuration);
 
 
 var app = builder.Build();
-
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI(options =>
-    {
-        options.SwaggerEndpoint("/swagger/v1/swagger.json", "StefanBeam API V1 Docs");
-        options.DocExpansion(DocExpansion.None);
-        options.DefaultModelsExpandDepth(-1); 
-    });
-    // Redirect to swagger page on application start
-    var option = new RewriteOptions();
-    option.AddRedirect("^$", "swagger");
-    app.UseRewriter(option);
-}
-
-app.UseHttpsRedirection();
-app.UseRouting();
-app.MapControllers();
+// Add all necesary config for the app (swagger, request pipeline, etc...)
+app.UseConfiguration();
 
 app.Run();
